5a9897062c31313458ce2d0a27a2158f5269a981
Shifana Nazeer <shifananazeer209@gmail.com>
2025-02-26 16:25:38 +0530

    backend for my articles

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -1,5 +1,6 @@
 import Article from "../models/Article.js";
 import Category from '../models/Category.js'
+import mongoose  from "mongoose";
 // Create a new article
 export const createArticle = async (req, res) => {
   try {
@@ -30,6 +31,28 @@ export const createArticle = async (req, res) => {
       res.json(categories);
     } catch (error) {
       console.error("Error fetching categories:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  export const getArticlesByUserId = async (req , res) => {
+    try {
+      const { userId } = req.params;
+
+      if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.status(400).json({ message: "Invalid user ID format" });
+      }
+  
+      // Find all articles where the author matches the given userId
+      const articles = await Article.find({ author: userId }).sort({ createdAt: -1 });
+  
+      if (!articles.length) {
+        return res.status(404).json({ message: "No articles found for this user." });
+      }
+  
+      res.status(200).json({ articles });
+    } catch (error) {
+      console.error("Error fetching articles:", error);
       res.status(500).json({ message: "Internal Server Error" });
     }
   }
--- backend/routes/articleRoute.js
+++ backend/routes/articleRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import upload from "../utils/upload.js"; 
-import { createArticle  , getCategories} from "../controllers/articleController.js";
+import { createArticle  , getCategories , getArticlesByUserId} from "../controllers/articleController.js";
 
 const router = express.Router();
 
 router.post("/create", upload.array("images", 5), createArticle); 
 router.get('/categories' , getCategories)
+router.get("/articles/:userId", getArticlesByUserId)
 
 export default router;
