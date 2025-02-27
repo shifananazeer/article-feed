7b482934fe5434fa159bd5a4f6203184bcd46bb3
Shifana Nazeer <shifananazeer209@gmail.com>
2025-02-27 16:38:28 +0530

    add test

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,14 @@ app.get("/api", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/articles", articleRoutes);
 
+app.get("/health", (req, res) => {
+  res.json({
+    message: "Server is running",
+    allowedOrigin: process.env.CLIENT_URL,
+    success: true,
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
